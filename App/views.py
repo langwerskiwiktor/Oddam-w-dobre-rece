aee948ddc087806c3769469e9916180014f98507
Witalix <langwerski@wp.pl>
2024-03-02 18:58:43 +0100

    Feature 2

--- App/views.py
+++ App/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from App.models import Institution, Donation
+
+
+class LandingPage(View):
+    def get(self, request):
+        bags_count = Donation.objects.count()
+        organizations_count = Institution.objects.count()
+        return render(request, 'index.html', {'bags_count': bags_count, 'organizations_count':organizations_count})
+
+
+class AddDonation(View):
+    def get(self, request):
+        return render(request, 'form.html')
+
+
+class LoginPage(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+
+class RegisterPage(View):
+    def get(self, request):
+        return render(request, 'register.html')
