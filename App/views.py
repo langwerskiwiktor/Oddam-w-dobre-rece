9263de11c9c7238e903c0e4ba56464977541a417
Witalix <langwerski@wp.pl>
2024-03-03 22:29:44 +0100

    Feature 3

--- App/views.py
+++ App/views.py
@@ -8,7 +8,11 @@ class LandingPage(View):
     def get(self, request):
         bags_count = Donation.objects.count()
         organizations_count = Institution.objects.count()
-        return render(request, 'index.html', {'bags_count': bags_count, 'organizations_count':organizations_count})
+        institutions = Institution.objects.filter(type=Institution.FUNDACJA)
+        organizations = Institution.objects.filter(type=Institution.ORGANIZACJA_POZARZĄDOWA)
+        locals = Donation.objects.all()
+        return render(request, 'index.html', {'bags_count': bags_count, 'organizations_count': organizations_count
+            , 'institutions': institutions, 'organizations': organizations, 'locals': locals})
 
 
 class AddDonation(View):
